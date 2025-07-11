ee31833f714e95e83d394fe276b9b6ab4f4cb774
Neeall <neeall.sevak@icloud.com>
2025-07-11 19:26:45 +0530

    here test fails due to not added , (comma) logic so, double digit won't be added

--- StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
@@ -14,8 +14,8 @@ final class StringCalculatorTests: XCTestCase {
     func testEmptyString(){
         let calculator = StringCalculator()
         XCTAssertEqual(calculator.add(""), 0)
-        XCTAssertEqual(calculator.add("7"), 7)
-        XCTAssertEqual(calculator.add("9"), 9)
+        XCTAssertEqual(calculator.add("1,5"), 6)
+        XCTAssertEqual(calculator.add("14,6"), 20)
     }
 
 }
