a78be3f66a5961b77f9d0c003ef77d310ca6ec9f
N <neeall.sevak@icloud.com>
2025-07-11 18:50:06 +0530

    Add files via upload
    //added basic test for empty string which successfully went ✅

---
+++ StringCalculator/StringCalculator/App/StringCalculator.swift
@@ -0,0 +1,16 @@
+//
+//  StringCalculator.swift
+//  StringCalculator
+//
+//  Created by Nilaykumar Sevak on 7/11/25.
+//
+
+import UIKit
+
+class StringCalculator{
+    func add(_ numbers:String)->Int{
+       
+        return 0
+    }
+}
+
---
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  StringCalculatorTests.swift
+//  StringCalculatorTests
+//
+//  Created by Nilaykumar Sevak on 7/11/25.
+//
+
+import XCTest
+@testable import StringCalculator
+
+final class StringCalculatorTests: XCTestCase {
+
+    //To check empty string
+    func testEmptyString(){
+        let calculator = StringCalculator()
+        XCTAssertEqual(calculator.add(""), 0)
+    }
+
+}
