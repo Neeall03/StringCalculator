85e3327289124c70ca81df17645172f7dfa5daf4
Neeall <neeall.sevak@icloud.com>
2025-07-11 22:24:15 +0530

    Now it will allow delimiter string and allow function to return sum/Interger

--- StringCalculator/StringCalculator/App/StringCalculator.swift
+++ StringCalculator/StringCalculator/App/StringCalculator.swift
@@ -13,12 +13,27 @@ class StringCalculator{
        guard !numbers.isEmpty else {
             return 0
         }
+        //taken as object for delimiter
+        var delimiter: Character = ","
+        //this will replace
+        var numbersToParse = numbers
+        // ; single char delimiter
+        if numbers.hasPrefix("//") {
+            if let firstNewlineIndex = numbers.firstIndex(of: "\n") {
+                let delimiterString = String(numbers[numbers.index(numbers.startIndex, offsetBy: 2)..<firstNewlineIndex])
+                if let customDel = delimiterString.first {
+                    delimiter = customDel
+                }
+                numbersToParse = String(numbers[numbers.index(after: firstNewlineIndex)...])
+            }
+        }
+        
         //debug for String
         print("Numbaer add -->",numbers)
         //this will replace \n (new line) with , (comma)
-        let cleanedNumbers = numbers.replacingOccurrences(of: "\n", with: ",")
+        let cleanedNumbers = numbersToParse.replacingOccurrences(of: "\n", with: String(delimiter))
         //this will remove ,
-        let components = cleanedNumbers.split(separator: ",").map(String.init)
+        let components = cleanedNumbers.split(separator: delimiter).map(String.init)
         //this sum of given numbers
         let sum = components.compactMap { Int($0) }.reduce(0, +)
                 return sum
